cff45110a312e4fb81308a598bdaba7905d787a0
yash <ym130602@gmail.com>
2026-04-05 03:27:35 +0530

    prisma setup completed with superbase and all-MiniLm-L6-v2 max tokens 256 is used for embedding vector

---
+++ lib/embedService.ts
@@ -0,0 +1,24 @@
+export const EmbedService = {
+
+  async generateEmbedding(text: string): Promise<number[]> {
+    const response = await fetch(
+      "https://router.huggingface.co/hf-inference/models/sentence-transformers/all-MiniLM-L6-v2/pipeline/feature-extraction",
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${process.env.HUGGINGFACE_API_KEY}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ inputs: text }),
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error(`Hugging Face API failed with status: ${response.status}`);
+    }
+
+    const vector = await response.json();
+ 
+    return Array.isArray(vector[0]) ? vector[0] : vector; 
+  }
+}
