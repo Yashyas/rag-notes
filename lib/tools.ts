698635e2d9310f39c9b226c8612f5cd5d99f4b4d
yash <ym130602@gmail.com>
2026-04-15 13:16:45 +0530

    tools added for the agentic workflow -vercel ai sdk

---
+++ lib/tools.ts
@@ -0,0 +1,44 @@
+import { z } from "zod";
+import { tool } from "ai";
+import { find_similar_notes } from "./similaritySearch";
+import { createNote, updateNoteDatabase } from "@/app/actions/notes";
+
+export const agentTools = {
+    // Semantic search 
+  semanticSearch: tool({
+    description:
+      "Search through the private notes of user to find relevant information using vector similarity.",
+    inputSchema: z.object({
+      query: z.string().describe("The search query for finding related notes"),
+    }),
+    execute: async ({query}) => {
+        const data = await find_similar_notes(query)
+        return data
+    }
+  }),
+
+  createNote: tool({
+    description: 'Create a new note for the user.',
+    inputSchema: z.object({
+      title: z.string().describe('Title of the note'),
+      content: z.string().describe('Body content of the note'),
+      tags: z.string().array().optional().describe('tags to better decribe,group and filter the note content'),
+    }),
+    execute: async ({title,content,tags}) =>{
+        return await createNote(content,title,tags ?? [])
+    }
+  }),
+
+   updateNote: tool({
+    description: 'Update an existing note for the user.',
+    inputSchema: z.object({
+      id: z.string().describe('ID of the note that is to be updated'),  
+      title: z.string().describe('Title of the note'),
+      content: z.string().describe('Body content of the note'),
+      tags: z.string().array().describe('tags to better decribe,group and filter the note content'),
+    }),
+    execute: async ({id,title,content,tags}) =>{
+        return await updateNoteDatabase(id,content,title,tags)
+    }
+  })
+};
